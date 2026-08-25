64afc8c1f1a6333ffc105da119690e9786cf667d
Karan Swansi <kswansi2002@gmail.com>
2026-08-25 10:15:05 -0400

    squared the checkboxes

--- resources/views/components/assignment-form.blade.php
+++ resources/views/components/assignment-form.blade.php
@@ -242,7 +242,7 @@
                                     >
                                         <input
                                             type="checkbox"
-                                            class="checkbox checkbox-md mt-0.5 shrink-0"
+                                            class="checkbox checkbox-md rounded-none mt-0.5 shrink-0"
                                             id="job-listing-{{ $job_listing->id }}"
                                             name="job_listing_ids[]"
                                             @checked(in_array($job_listing->id, 
