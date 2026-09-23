48a0a57ebf04774ecc5dc75eb05a31221b8bb4f7
Karan Swansi <kswansi2002@gmail.com>
2026-09-22 20:38:52 -0400

    Response failed test

--- tests/Feature/EvaluateJobTest.php
+++ tests/Feature/EvaluateJobTest.php
@@ -5,10 +5,7 @@ use App\Jobs\EvaluateJob;
 use App\Models\Evaluation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\Storage;
-
-$jobDescription = "test job description";
 
 uses(RefreshDatabase::class);
 
@@ -72,4 +69,37 @@ test('Failed connection.', function () {
         ->and($evaluation->failure_reason)->toBe('Evaluation service seems to be down.')
         ->and($evaluation->resume_text)->toBe('')
         ->and($evaluation->evaluation_data)->toBeNull();
+});
+
+
+test('Response Failed.', function () {
+    Http::fake([
+        '*evaluate' => Http::response([
+            'resume_text' => 'partial text',
+        ], 500)
+    ]);
+
+    $evaluation = Evaluation::factory()
+        ->withStatus(EvaluationStatus::Processing)
+        ->create();
+
+    Storage::disk('local')->put(
+        'resumes/tmp/sample-resume.pdf',
+        file_get_contents(evaluationFixture('sample-resume.pdf'))
+    ); 
+
+    EvaluateJob::dispatchSync( // or ->handle() — see below
+        'resumes/tmp/sample-resume.pdf',
+        'test job description',
+        $evaluation
+    );
+
+    $evaluation->refresh();
+
+    expect($evaluation->status)->toBe(EvaluationStatus::Failed)
+        ->and($evaluation->failure_reason)->toBe('Evaluation Service was unable to process your request.')
+        ->and($evaluation->resume_text)->toBe('partial text')
+        ->and($evaluation->evaluation_data)->toMatchArray([
+            'resume_text' => 'partial text'
+        ]);
 });
